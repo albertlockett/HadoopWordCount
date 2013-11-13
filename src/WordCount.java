6719c214a416ce2c9f876bd81786706fb6f3bf19
Albert Lockett <albert.lockett@ambirsolutions.com>
2013-11-13 09:05:23 -0400

    first commit

---
+++ src/WordCount.java
@@ -0,0 +1,51 @@
+
+
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCount extends Configured implements Tool{
+
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        if(args.length != 2){
+            System.err.printf("Usage: Hadoop jar %s.jar [generic options] <input> <output>",
+                    getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Job job = new Job(getConf(), "Word Count");
+        job.setJarByClass(getClass());
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class);
+        job.setCombinerClass(WordCountReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        return job.waitForCompletion(true) ? 0: 1;
+
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        int exitCode = ToolRunner.run(new WordCount(), args);
+        System.exit(exitCode);
+    }
+
+
+}
